e952279f2401bfa5d40c4d43e84111893ea64455
Jason Mulligan <jason.mulligan@avoidwork.com>
2016-07-13 23:11:47 -0400

    Removing an unused module

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const //weather = require("canada-weather"),
 	mkdirp = require("mkdirp"),
 	path = require("path"),
-	jsonpath = require("jsonpath").query,
+	JSONPath = require("JSONPath"),
 	root = path.join(__dirname, "data"),
 	config = require(path.join(__dirname, "config.json")),
 	pkg = require(path.join(__dirname, "package.json")),
@@ -42,16 +42,16 @@ function datum (key) {
 		},
 		msg, match;
 
-	match = jsonpath(weather, path[key])[0] || null;
+	match = JSONPath({json: weather, path: path[key]})[0] || null;
 
 	if (key === "up") {
 		msg = [match ? match.event.description : "No warning"];
 	} else if (key === "down") {
-		msg = [match ? "Updated " + moment.unix(match / 1000).fromNow() : " some time ago"];
+		msg = [match ? "Updated " + moment.unix(match / 1000).fromNow() + " some time ago" : "Something went wrong"];
 	} else if (key === "left") {
-		msg = match ? match[0] : "Something went wrong";
+		msg = [match ? match[0] : "Something went wrong"];
 	} else if (key === "right") {
-		msg = match ?["Updated " + moment.unix(match / 1000).fromNow()] : "Something went wrong";
+		msg = [match ? "Updated " + moment.unix(match / 1000).fromNow() : "Something went wrong"];
 	}
 
 	if (msg) {
